3272a5d1e652afdc7d44371254fe7c884614a9f3
Dyrax <22845385+Dyrax@users.noreply.github.com>
2023-04-21 19:46:59 +0200

    Disable menu items

--- AutoSplitter/lua/menu.lua
+++ AutoSplitter/lua/menu.lua
@@ -1,4 +1,14 @@
 if RequiredScript == "lib/managers/menumanager" then
+	function disable_items()
+		local menu = MenuHelper:GetMenu("autosplitter_menu")
+		for _, item in pairs(menu._items) do
+			local name = item._parameters.name
+			if name == "autosplitter_igt_on_restarts" or name == "autosplitter_round_igt" then
+				item:set_enabled(AutoSplitter._data.game_time_mode == AutoSplitter._game_time_modes.HeistTime)
+			end
+		end
+	end
+
 	Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_AutoSplitter", function( loc )
 		loc:load_localization_file( AutoSplitter._path .. "lua/loc/english.json")
 	end)
@@ -12,7 +22,9 @@ if RequiredScript == "lib/managers/menumanager" then
 		MenuCallbackHandler.callback_autosplitter_game_time_mode = function(self, item)
 			AutoSplitter._data.game_time_mode = item:value()
 			AutoSplitter:SaveSettings()
+			disable_items()
 		end
+		
 		MenuCallbackHandler.callback_autosplitter_igt_on_restarts = function(self, item)
 			AutoSplitter._data.igt_on_restarts = (item:value() == "on" and true or false)
 			AutoSplitter:SaveSettings()
@@ -45,5 +57,8 @@ if RequiredScript == "lib/managers/menumanager" then
 
 		AutoSplitter:LoadSettings()
 		MenuHelper:LoadFromJsonFile( AutoSplitter._path .. "lua/menu.json", AutoSplitter, AutoSplitter._data )
+		Hooks:Add("MenuManagerBuildCustomMenus","autosplitter_disable_items", function(menu_manager, nodes)
+			disable_items()
+		end)
 	end )
 end
